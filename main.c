9146eb0b139b660e61939324eef0b57b86d1361a
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-09-15 16:08:13 +0600

    v1.1.2

--- main.c
+++ main.c
@@ -2,9 +2,23 @@
 
 int main(){
     int a = 5;
-    int b = 10;
-    int c = a+b;
+    float b = 5.6;
+    double c = 7.8;
+    char C = 'M';
+    long l = 23;
+    short s = 2;
 
-    printf("%d\n", c);
+    printf("%d\n", a);  // print int 
+
+    printf("%lf\n", b);    // print float
+
+    printf("%f\n", c);      // print double
+
+    printf("%c\n", C);      //  print char
+
+    printf("%ld\n", l);     //  print long
+
+    printf("%d\n", s);      //print short
+
     
 }

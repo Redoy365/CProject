1fdd906b3f7df6bd13e639e48f0ffa9239c932f0
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-09-15 17:49:55 +0600

    v1.1.6

--- main.c
+++ main.c
@@ -1,26 +1,10 @@
 #include<stdio.h>
+
 int main(){
-
-    int a = 10;
-    int b = 2;
-
-    int sum,sub,mul;
-    float div;
-    char str;
-    char text;
     
-    sum = a+b;
-    sub = a-b;
-    mul = a*b;
-    div = a/b;
-    str = 'a';
-    text["Hello Text"];
+    const int a=4;
 
-    printf("%d\n", sum);
-    printf("%d\n", sub);
-    printf("%d\n", mul);
-    printf("%f\n", div);
-    printf("%c\n", str);
-    printf("%s\n", text);
+    printf("%d\n", a);
 
+    
 }

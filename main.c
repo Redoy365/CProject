78e06ff14f895c459b6cab8be048b90ac7da0cae
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-10-10 15:43:21 +0600

    v1.2.6

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
-// Working of increment and decrement operators
 #include <stdio.h>
-int main()
-{
-    int a = 10, b = 100;
-    float c = 10.5, d = 100.5;
-
-    printf("++a = %d \n", ++a);
-    printf("--b = %d \n", --b);
-    printf("++c = %f \n", ++c);
-    printf("--d = %f \n", --d);
-    printf("\n");
-    printf("++a = %d \n", a++);
-    printf("--b = %d \n", b--);
-    printf("++c = %f \n", c++);
-    printf("--d = %f \n", d--);
-
+int main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
     return 0;
 }
+
+/*
+
+git add .
+git status
+git log -1
+git commit - m ""
+
+*/
